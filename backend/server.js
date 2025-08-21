0ce9b3cd7495790723ac4beeb72e7d2ac02eb2ce
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-20 23:56:22 -0400

    added route

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/recipes", (req, res) => {
+    res.send("Here are the recipes");
+});
+
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001");
 })
