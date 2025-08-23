c51d00c5219837e2e771c770e70daf87ad2a3080
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-23 02:14:23 -0400

    created recipe schema

---
+++ backend/models/recipe.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const recipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: false,
+  },
+  servings: {
+    type: String,
+    required: false,
+  },
+  ingredients: {
+    type: [String],
+    required: true,
+  },
+  instructions: {
+    type: [String],
+    required: true,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+});
