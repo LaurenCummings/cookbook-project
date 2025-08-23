52c42fcb63942b723cb1ca8af4183f8c73bc026d
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-23 02:56:20 -0400

    added success and failure messages when creating new recipe

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,16 @@ app.post("/recipes", async (req, res) => {
       .status(400)
       .json({ success: false, message: "Please provide all fields" });
   }
+
+  const newRecipe = new Recipe(recipe);
+
+  try {
+    await newRecipe.save();
+    res.status(201).json({ success: true, data: newRecipe });
+  } catch (error) {
+    console.error("Error in Create recipe:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 app.listen(5000, () => {
