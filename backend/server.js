f81eb9446df2af172128d0961572daec3fdd1017
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-23 02:46:18 -0400

    added check for required fields

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,16 @@ app.get("/recipes", (req, res) => {
   res.send("Here are the recipes");
 });
 
+app.post("/recipes", async (req, res) => {
+  const recipe = req.body;
+
+  if (!recipe.name || !recipe.ingredients || !recipe.instructions) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+});
+
 app.listen(5000, () => {
   connectDB();
   console.log("Server started at http://localhost:5000");
