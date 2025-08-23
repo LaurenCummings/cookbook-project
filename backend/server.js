e1dd129656c4672efc22aca11eb1f5b1abd94d88
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-23 00:08:19 -0400

    called database connection function from server file

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
-
-console.log(process.env.MONGO_URI);
 
 app.get("/recipes", (req, res) => {
   res.send("Here are the recipes");
 });
 
 app.listen(5000, () => {
+  connectDB();
   console.log("Server started at http://localhost:5000");
 });
