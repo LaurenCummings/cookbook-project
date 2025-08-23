0aaf688eec824a75de65ea2eb93d4574f168c465
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-23 02:15:49 -0400

    added timestamps to recipe schema

--- backend/models/recipe.model.js
+++ backend/models/recipe.model.js
@@ -1,28 +1,33 @@
 import mongoose from "mongoose";
 
-const recipeSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const recipeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: false,
+    },
+    servings: {
+      type: String,
+      required: false,
+    },
+    ingredients: {
+      type: [String],
+      required: true,
+    },
+    instructions: {
+      type: [String],
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
   },
-  time: {
-    type: String,
-    required: false,
-  },
-  servings: {
-    type: String,
-    required: false,
-  },
-  ingredients: {
-    type: [String],
-    required: true,
-  },
-  instructions: {
-    type: [String],
-    required: true,
-  },
-  image: {
-    type: String,
-    required: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
