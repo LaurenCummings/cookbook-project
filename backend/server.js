58fbe247a4c8e81f26c4f55a8386df8bd8dc6f27
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-22 23:35:55 -0400

    changed order so route comes after app is initialized

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const app = express();
+
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
-
-const app = express();
 
 app.listen(5000, () => {
   console.log("Server started at http://localhost:5000");
