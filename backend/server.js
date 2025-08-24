293855f67a85f615da720ad4032e8f6b1581cdae
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-24 02:30:23 -0400

    imported mongoose into server file to check for bad id type

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import { connectDB } from "./config/db.js";
 import Recipe from "./models/recipe.model.js";
 
