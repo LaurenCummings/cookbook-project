28277067f279b6799501f14f2dd0cf18b3658b35
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-25 17:14:50 -0400

    added content and styling to createPage

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,4 +1,11 @@
 import { useState } from "react";
+import {
+  Container,
+  VStack,
+  Heading,
+  Box,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 const CreatePage = () => {
   const [newRecipe, setNewRecipe] = useState({
@@ -6,7 +13,23 @@ const CreatePage = () => {
     ingredients: "",
     instructions: "",
   });
-  return <div>CreatePage</div>;
+  return (
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
+
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        ></Box>
+      </VStack>
+    </Container>
+  );
 };
 
 export default CreatePage;
