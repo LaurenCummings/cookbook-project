abc8f3e423bb844ef9544d9f9d2f0e93b397c0fd
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-25 15:35:37 -0400

    added route for create page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
       </Routes>
     </Box>
   );
