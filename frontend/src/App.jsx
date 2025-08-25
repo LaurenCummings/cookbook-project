e9bd142aa4da79122a1fddc2d42ae28ef37aafa9
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-25 15:25:56 -0400

    added homepage route to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <Box minH={"100vh"}>{/* <Navbar /> */}</Box>;
+  return (
+    <Box minH={"100vh"}>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
