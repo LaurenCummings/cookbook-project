727b3b4090870ab0471251f733e5cd87a8b74a56
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-26 23:50:05 -0400

    made function to create new recipes

--- frontend/src/store/recipe.js
+++ frontend/src/store/recipe.js
@@ -3,4 +3,10 @@ import { create } from "zustand";
 export const useRecipeStore = create((set) => ({
   recipes: [],
   setRecipes: (recipes) => set({ recipes }),
+  createRecipe: async (newRecipe) => {
+    if (!newRecipe.name || !newRecipe.ingredients || !newRecipe.instructions) {
+      return { success: false, message: "Please fill in all required fields." };
+    }
+    const res = await fetch("http://localhost:5000/api/recipes");
+  },
 }));
