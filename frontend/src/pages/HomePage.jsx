eb8d496602e6e2dc4f2c91bd1e0f085fcc3e98bc
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-27 23:39:23 -0400

    added styling to home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,9 +2,17 @@ import { Container, Text, VStack } from "@chakra-ui/react";
 
 const HomePage = () => {
   return (
-    <Container>
-      <VStack>
-        <Text></Text>
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Recipes
+        </Text>
       </VStack>
     </Container>
   );
