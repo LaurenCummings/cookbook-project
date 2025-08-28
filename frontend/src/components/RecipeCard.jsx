561920d2454b68a55d64784c9a165aa5f71ae0b2
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-28 00:53:39 -0400

    added icons to RecipeCard

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -1,4 +1,5 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Text, HStack, IconButton } from "@chakra-ui/react";
+import { EditIcon, DeleteIcon } from "react-icons";
 
 const RecipeCard = ({ recipe }) => {
   return (
@@ -6,6 +7,16 @@ const RecipeCard = ({ recipe }) => {
       <Heading as="h3" size="md" mb={2}>
         {recipe.name}
       </Heading>
+      <Text fontWeight="bold" fontSize="xl" color={textColor} mb={4}>
+        {recipe.ingredients}
+      </Text>
+      <HStack spacing={2}>
+        <IconButton icon={<EditIcon />} onClick={onOpen} colorScheme="blue" />
+        <IconButton
+          icon={<DeleteIcon />}
+          onClick={() => handleDelete(recipe._id)}
+        />
+      </HStack>
     </Box>
   );
 };
