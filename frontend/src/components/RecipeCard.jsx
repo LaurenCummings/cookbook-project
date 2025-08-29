fe5112d8bf7080dafbc77500e52f3edb14aa8292
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-28 23:54:29 -0400

    imported deleteRecipe into recipeCard

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -12,6 +12,13 @@ import { useRecipeStore } from "../store/recipe";
 const RecipeCard = ({ recipe }) => {
   const textColor = useColorModeValue("gray.600", "gray.200");
   const bg = useColorModeValue("white", "gray.800");
+
+  const { deleteRecipe } = useRecipeStore();
+
+  const handleDeleteRecipe = async (rid) => {
+    const { success, message } = await deleteRecipe(rid);
+  };
+
   return (
     <Box p={4} bg={bg}>
       <Heading as="h3" size="md" mb={2}>
