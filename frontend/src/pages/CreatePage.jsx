808c2bc9c9486e9bf1f8ad17b9636a3b859efbb6
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-29 23:28:08 -0400

    changed create page to say 'create new recipe'

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -45,7 +45,7 @@ const CreatePage = () => {
     <Container maxW={"container.sm"}>
       <VStack spacing={8}>
         <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
-          Create New Product
+          Create New Recipe
         </Heading>
 
         <Box
