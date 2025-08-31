71c577b026768e4ef9ccbef3e73122363b2f275b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-31 00:05:47 -0400

    added ingredients heading and resized headings

--- frontend/src/pages/RecipePage.jsx
+++ frontend/src/pages/RecipePage.jsx
@@ -7,10 +7,13 @@ const RecipePage = () => {
   return (
     <Container maxW={"container.sm"}>
       <VStack spacing={8}>
-        <Heading as={"h1"} size={"xl"} textAlign={"center"} mb={8}>
+        <Heading as={"h1"} size={"lg"} textAlign={"center"} mb={8}>
           {recipe.name}
         </Heading>
         <Container>
+          <Heading as={"h3"} size={"md"}>
+            Ingredients
+          </Heading>
           {recipe.ingredients.split("\n").map((item, index) => (
             <p key={index}>{item}</p>
           ))}
