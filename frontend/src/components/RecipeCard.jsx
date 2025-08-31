c5a207f2085ef52f27a85f053b6c4700dc024904
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-30 23:09:25 -0400

    added link to recipePage to recipeCard

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -77,86 +77,92 @@ const RecipeCard = ({ recipe }) => {
   };
 
   return (
-    <Box
-      shadow="lg"
-      rounded="lg"
-      overflow="hidden"
-      transition="all 0.3s"
-      _hover={{ transform: "translateY(-5px)", shadow: "xl" }}
-      bg={bg}
-    >
-      <Box p={4} bg={bg}>
-        <Heading as="h3" size="md" mb={2}>
-          {recipe.name}
-        </Heading>
-        <Text fontWeight="bold" fontSize="xl" color={textColor} mb={4}>
-          {recipe.ingredients}
-        </Text>
-        <HStack spacing={2}>
-          <IconButton icon={<EditIcon />} onClick={onOpen} colorScheme="blue" />
-          <IconButton
-            icon={<DeleteIcon />}
-            onClick={() => handleDeleteRecipe(recipe._id)}
-            colorScheme="red"
-          />
-        </HStack>
-      </Box>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Update Recipe</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <VStack spacing={4}>
-              <Input
-                placeholder="Recipe Name"
-                name="name"
-                value={updatedRecipe.name}
-                onChange={(e) =>
-                  setUpdatedRecipe({ ...updatedRecipe, name: e.target.value })
-                }
-              />
-              <Textarea
-                placeholder="Ingredients"
-                name="ingredients"
-                value={updatedRecipe.ingredients}
-                onChange={(e) =>
-                  setUpdatedRecipe({
-                    ...updatedRecipe,
-                    ingredients: e.target.value,
-                  })
-                }
-              />
-              <Textarea
-                placeholder="Instructions"
-                name="instructions"
-                value={updatedRecipe.instructions}
-                onChange={(e) =>
-                  setUpdatedrecipe({
-                    ...updatedRecipe,
-                    instructions: e.target.value,
-                  })
-                }
-              />
-            </VStack>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button
+    <Link to="/RecipePage/:id" recipe={recipe}>
+      <Box
+        shadow="lg"
+        rounded="lg"
+        overflow="hidden"
+        transition="all 0.3s"
+        _hover={{ transform: "translateY(-5px)", shadow: "xl" }}
+        bg={bg}
+      >
+        <Box p={4} bg={bg}>
+          <Heading as="h3" size="md" mb={2}>
+            {recipe.name}
+          </Heading>
+          <Text fontWeight="bold" fontSize="xl" color={textColor} mb={4}>
+            {recipe.ingredients}
+          </Text>
+          <HStack spacing={2}>
+            <IconButton
+              icon={<EditIcon />}
+              onClick={onOpen}
               colorScheme="blue"
-              mr={3}
-              onClick={() => handleUpdateRecipe(recipe._id, updatedRecipe)}
-            >
-              Update
-            </Button>
-            <Button variant="ghost" onClick={onClose}>
-              Cancel
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </Box>
+            />
+            <IconButton
+              icon={<DeleteIcon />}
+              onClick={() => handleDeleteRecipe(recipe._id)}
+              colorScheme="red"
+            />
+          </HStack>
+        </Box>
+
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Update Recipe</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <VStack spacing={4}>
+                <Input
+                  placeholder="Recipe Name"
+                  name="name"
+                  value={updatedRecipe.name}
+                  onChange={(e) =>
+                    setUpdatedRecipe({ ...updatedRecipe, name: e.target.value })
+                  }
+                />
+                <Textarea
+                  placeholder="Ingredients"
+                  name="ingredients"
+                  value={updatedRecipe.ingredients}
+                  onChange={(e) =>
+                    setUpdatedRecipe({
+                      ...updatedRecipe,
+                      ingredients: e.target.value,
+                    })
+                  }
+                />
+                <Textarea
+                  placeholder="Instructions"
+                  name="instructions"
+                  value={updatedRecipe.instructions}
+                  onChange={(e) =>
+                    setUpdatedrecipe({
+                      ...updatedRecipe,
+                      instructions: e.target.value,
+                    })
+                  }
+                />
+              </VStack>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button
+                colorScheme="blue"
+                mr={3}
+                onClick={() => handleUpdateRecipe(recipe._id, updatedRecipe)}
+              >
+                Update
+              </Button>
+              <Button variant="ghost" onClick={onClose}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Box>
+    </Link>
   );
 };
 
