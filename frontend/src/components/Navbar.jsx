40dd6088842a82e7f33ba8ec9d4babc9a18ab297
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-31 00:07:34 -0400

    changed navbar to say my cookbook

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
           bgGradient={"linear(to-r, cyan.400, blue.500)"}
           bgClip={"text"}
         >
-          <Link to={"/"}>Product Store 🛒</Link>
+          <Link to={"/"}>My Cookbook</Link>
         </Text>
 
         <HStack spacing={2} alignItems={"center"}>
