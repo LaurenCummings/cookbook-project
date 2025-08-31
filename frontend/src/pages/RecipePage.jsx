cb9213eba6f8b3edec590eec49c2c46f5c5f6b2f
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-31 00:13:04 -0400

    moved ingredients to center

--- frontend/src/pages/RecipePage.jsx
+++ frontend/src/pages/RecipePage.jsx
@@ -7,15 +7,15 @@ const RecipePage = () => {
   return (
     <Container maxW={"container.sm"}>
       <VStack spacing={8}>
-        <Heading as={"h1"} size={"lg"} textAlign={"center"} mb={8}>
+        <Heading as={"h1"} size={"lg"} textAlign={"center"}>
           {recipe.name}
         </Heading>
         <Container>
-          <Heading as={"h3"} size={"md"}>
+          <Heading as={"h3"} size={"md"} textAlign={"center"}>
             Ingredients
           </Heading>
           {recipe.ingredients.split("\n").map((item, index) => (
-            <Text key={index} fontSize="sm">
+            <Text key={index} fontSize="sm" textAlign={"center"}>
               {item}
             </Text>
           ))}
