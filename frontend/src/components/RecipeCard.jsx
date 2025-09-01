bb5f363f4f1ae181b275d553cbd343c7353c5021
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-01 17:00:55 -0400

    deleted edit and delete functionality and icons from home page

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -1,81 +1,9 @@
-import {
-  Box,
-  Button,
-  Heading,
-  Text,
-  HStack,
-  VStack,
-  IconButton,
-  Image,
-  useColorModeValue,
-  useToast,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  ModalFooter,
-  useDisclosure,
-  Input,
-  Textarea,
-} from "@chakra-ui/react";
-import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
-import { useRecipeStore } from "../store/recipe";
-import { useState } from "react";
+import { Box, Heading, Text, Image, useColorModeValue } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const RecipeCard = ({ recipe }) => {
-  const [updatedRecipe, setUpdatedRecipe] = useState(recipe);
   const textColor = useColorModeValue("gray.600", "gray.200");
   const bg = useColorModeValue("white", "gray.800");
-
-  const { deleteRecipe, updateRecipe } = useRecipeStore();
-  const toast = useToast();
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const handleDeleteRecipe = async (rid) => {
-    const { success, message } = await deleteRecipe(rid);
-    if (!success) {
-      toast({
-        title: "Error",
-        description: message,
-        status: "error",
-        duration: 3000,
-        isClosable: true,
-      });
-    } else {
-      toast({
-        title: "Success",
-        description: message,
-        status: "success",
-        duration: 3000,
-        isClosable: true,
-      });
-    }
-  };
-
-  const handleUpdateRecipe = async (rid, updatedRecipe) => {
-    const { success, message } = await updateRecipe(rid, updatedRecipe);
-    onClose();
-    if (!success) {
-      toast({
-        title: "Error",
-        description: message,
-        status: "error",
-        duration: 3000,
-        isClosable: true,
-      });
-    } else {
-      toast({
-        title: "Success",
-        description: "Recipe updated successfully",
-        status: "success",
-        duration: 3000,
-        isClosable: true,
-      });
-    }
-  };
 
   return (
     <Link to={`/recipePage/${recipe._id}`} state={recipe}>
@@ -101,74 +29,7 @@ const RecipeCard = ({ recipe }) => {
           <Text fontWeight="bold" fontSize="xl" color={textColor} mb={4}>
             {recipe.ingredients}
           </Text>
-          <HStack spacing={2}>
-            <IconButton
-              icon={<EditIcon />}
-              onClick={onOpen}
-              colorScheme="blue"
-            />
-            <IconButton
-              icon={<DeleteIcon />}
-              onClick={() => handleDeleteRecipe(recipe._id)}
-              colorScheme="red"
-            />
-          </HStack>
         </Box>
-
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Update Recipe</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <VStack spacing={4}>
-                <Input
-                  placeholder="Recipe Name"
-                  name="name"
-                  value={updatedRecipe.name}
-                  onChange={(e) =>
-                    setUpdatedRecipe({ ...updatedRecipe, name: e.target.value })
-                  }
-                />
-                <Textarea
-                  placeholder="Ingredients"
-                  name="ingredients"
-                  value={updatedRecipe.ingredients}
-                  onChange={(e) =>
-                    setUpdatedRecipe({
-                      ...updatedRecipe,
-                      ingredients: e.target.value,
-                    })
-                  }
-                />
-                <Textarea
-                  placeholder="Instructions"
-                  name="instructions"
-                  value={updatedRecipe.instructions}
-                  onChange={(e) =>
-                    setUpdatedRecipe({
-                      ...updatedRecipe,
-                      instructions: e.target.value,
-                    })
-                  }
-                />
-              </VStack>
-            </ModalBody>
-
-            <ModalFooter>
-              <Button
-                colorScheme="blue"
-                mr={3}
-                onClick={() => handleUpdateRecipe(recipe._id, updatedRecipe)}
-              >
-                Update
-              </Button>
-              <Button variant="ghost" onClick={onClose}>
-                Cancel
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
       </Box>
     </Link>
   );
