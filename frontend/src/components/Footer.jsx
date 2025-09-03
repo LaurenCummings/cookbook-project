85fb00033f95c5d89a35c05576cf62c98db15bf8
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-02 23:54:40 -0400

    added icons8 ref to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,17 @@
+import Text from "@chakra-ui/react";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Text>
+      <a target="_blank" href="https://icons8.com/icon/JlAUuOdwMf2P/whisk">
+        Whisk
+      </a>{" "}
+      icon by{" "}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+    </Text>
+  );
 };
 
 export default Footer;
