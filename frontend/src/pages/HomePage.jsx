5bd780b2c8f5863239809f7ab5cd25b6cf691e30
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-03 00:57:05 -0400

    deleted current recipes label

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -15,16 +15,6 @@ const HomePage = () => {
   return (
     <Container maxW="container.xl" py={12}>
       <VStack spacing={8}>
-        <Text
-          fontSize={"30"}
-          fontWeight={"bold"}
-          bgGradient={"linear(to-r, cyan.400, blue.500)"}
-          bgClip={"text"}
-          textAlign={"center"}
-        >
-          Current Recipes
-        </Text>
-
         <SimpleGrid
           columns={{
             base: 1,
@@ -33,6 +23,7 @@ const HomePage = () => {
           }}
           spacing={10}
           w={"full"}
+          mt={10}
         >
           {recipes.map((recipe) => (
             <RecipeCard key={recipe._id} recipe={recipe} />
