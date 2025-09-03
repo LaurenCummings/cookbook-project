c148b08d45bd6d4d4bf872bd48ac95fd9123c836
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-03 01:11:03 -0400

    imported path package and created dirname constant

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 
 import recipeRoutes from "./routes/recipe.route.js";
@@ -8,6 +9,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 
