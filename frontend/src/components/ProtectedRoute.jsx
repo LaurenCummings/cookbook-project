ee8f2744826c96ff28493014853f179a61e5d9c0
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-07 02:07:17 -0400

    added toast message when user isn't logged in

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,7 +1,17 @@
 import { Navigate } from "react-router-dom";
+import { useToast } from "@chakra-ui/react";
 
 const ProtectedRoute = ({ children, isAuthenticated }) => {
+  const toast = useToast();
+
   if (!isAuthenticated) {
+    toast({
+      title: "Error",
+      description: "You must be logged in to view recipes",
+      status: "error",
+      duration: 3000,
+      isClosable: true,
+    });
     return <Navigate to="/" replace />;
   }
   return children;
