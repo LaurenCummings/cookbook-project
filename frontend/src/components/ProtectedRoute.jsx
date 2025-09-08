b7810a8cfe0fdcefd28f1e8aed67481941cab794
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-08 00:02:55 -0400

    removed toast message from authentication check

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,18 +1,9 @@
 import { Navigate } from "react-router-dom";
-import { useToast } from "@chakra-ui/react";
 
 const ProtectedRoute = ({ children, isAuthenticated }) => {
   const toast = useToast();
 
   if (!isAuthenticated) {
-    toast.closeAll();
-    toast({
-      title: "Error",
-      description: "You must be logged in to create recipes",
-      status: "error",
-      duration: 3000,
-      isClosable: true,
-    });
     return <Navigate to="/" replace />;
   }
   return children;
