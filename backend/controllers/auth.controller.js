6f917d899cbcc755c71e7d1e9d6a2815a2c3a9e0
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-11 02:24:08 -0400

    added login success message

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -9,7 +9,7 @@ export const login = (req, res) => {
       return res.status(400).json({ message: "Invalid credentials" });
     }
     generateToken();
-    res.status(200);
+    res.status(200).json({ message: "Login successful" });
   } catch (error) {
     console.log("Error in login controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
