7a358ecfa82b19a922b691e68acea38456f03489
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-13 00:49:52 -0400

    added check for valid token

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -10,6 +10,12 @@ export const protectRoute = async (req, res, next) => {
         .json({ message: "Unauthorized - No Token Provided" });
     }
 
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized - Invalid Token" });
+    }
+
     next();
   } catch (error) {
     console.log("Error in protectRoute middleware: ", error.message);
