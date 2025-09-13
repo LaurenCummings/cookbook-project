dc5b2d0822cc86b93a2129dd961550a431653eee
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-13 00:47:02 -0400

    created protectRoute middlware

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - No Token Provided" });
+    }
+
+    next();
+  } catch (error) {
+    console.log("Error in protectRoute middleware: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
