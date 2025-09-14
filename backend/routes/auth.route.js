9955830d5de78dc60ce7b2da87c0b4dbcd178339
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-14 00:18:18 -0400

    added check route for authentication

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { login, logout } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.get("/check", protectRoute);
 
 export default router;
