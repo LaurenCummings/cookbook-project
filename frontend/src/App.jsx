649ab9130399ee538d2013afb93461c4b3bd6f94
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-14 00:29:02 -0400

    deleted old authentication system

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,33 +5,16 @@ import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import ProtectedRoute from "./components/ProtectedRoute";
 import { useState } from "react";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
-
-  const handleLogin = () => {
-    setIsAuthenticated(true);
-  };
-
   return (
     <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
-      <Navbar isAuthenticated={isAuthenticated} />
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route
-          path="/create"
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
-              <CreatePage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/recipePage/:id"
-          element={<RecipePage isAuthenticated={isAuthenticated} />}
-        />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/recipePage/:id" element={<RecipePage />} />
       </Routes>
       <Footer />
     </Box>
