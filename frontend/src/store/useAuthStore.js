cd7f1a7164841de860e9a54d051a2fc45cd0824f
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-14 00:37:10 -0400

    made authUser false on logout

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -35,6 +35,7 @@ export const useAuthStore = create((set, get) => ({
   logout: async () => {
     try {
       await axiosInstance.post("/auth/logout");
+      set({ authUser: false });
       console.log("Logged out");
     } catch (error) {
       console.log(error.response.data.message);
