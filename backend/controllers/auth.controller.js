54988b3b71ebf33f6091ed34f561ba0d81b868cb
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-14 20:42:55 -0400

    added password printout for debugging purposes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,6 +4,8 @@ export const login = (req, res) => {
   const { password } = req.body;
   try {
     const isPasswordCorrect = password === process.env.PASSWORD;
+    console.log("Entered password: ", password);
+    console.log("Expected password: ", process.env.PASSWORD);
 
     if (!isPasswordCorrect) {
       return res.status(400).json({ message: "Invalid credentials" });
